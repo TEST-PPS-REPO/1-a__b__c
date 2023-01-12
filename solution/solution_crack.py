f77f1f8fa187670a8b2a3ccf7da5cfad541296d0
test-pps-admin <test.pps.github@gmail.com>
2023-01-13 02:37:27 +0900

    Update: path 'solution/solution_crack.py' - Updated by 'thak00'

--- solution/solution_crack.py
+++ solution/solution_crack.py
@@ -5,4 +5,4 @@ for i in range(8):
   with open('IO/in_{:04d}.txt'.format(i)) as f:
     if f.read().strip() == inp:
       with open('IO/out_{:04d}.txt'.format(i)) as f2:
-        print(f2.read())
+        print(f2.read().strip())
