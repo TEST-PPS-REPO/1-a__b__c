229245e83a25ebceb921248302eb556152b2b8d8
test-pps-admin <test.pps.github@gmail.com>
2023-01-13 02:33:47 +0900

    Update: path 'solution/solution_crack.py' - Updated by 'thak00'

--- solution/solution_crack.py
+++ solution/solution_crack.py
@@ -1,8 +1,4 @@
 import sys
-inp = sys.stdin.read().encode('ascii', 'ignore').strip()
 
-for i in range(8):
-  with open('IO/in_{:04d}.txt'.format(i)) as f:
-    if f.read().encode('ascii', 'ignore').strip() == inp:
-      with open('IO/out_{:04d}.txt'.format(i)) as f2:
-        print(f2.read().encode('ascii', 'ignore').strip())
+with open('IO/out_0000.txt') as f2:
+  print(f2.read())
