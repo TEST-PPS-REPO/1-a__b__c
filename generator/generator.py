d1f6cbe705fd23419cf913405a5f795bdccf9436
test-pps-admin <test.pps.github@gmail.com>
2023-01-20 14:49:25 +0900

    Update: path 'generator/generator.py' - Updated by 'thak00'

--- generator/generator.py
+++ generator/generator.py
@@ -11,5 +11,9 @@ random.seed(hashValue)
 l = int(sys.argv[1])
 r = int(sys.argv[2])
 
-a, b, c = [rnd(l, r) for _ in range(2389472938472347243)]
+ar = []
+for _ in range(101010):
+  ar.append([123] * 384724)
+
+a, b, c = [rnd(l, r) for _ in range(3)]
 print(a, b, c)
