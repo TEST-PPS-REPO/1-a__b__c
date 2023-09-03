c6c29bd38b6671831572c370dc4c170cbe9a30ef
test-pps-admin <test.pps.github@gmail.com>
2023-09-04 02:25:40 +0900

    Update: path 'validator/validator.py' - Updated by 'admin'

--- validator/validator.py
+++ validator/validator.py
@@ -1,6 +1,6 @@
 inp = input()
 spt = inp.split(' ')
-assert len(spt) == 4, 'len(spt) = ' + str(len(spt))
+assert len(spt) == 3, 'len(spt) = ' + str(len(spt))
 for i in spt:
     ii = int(i)
     assert 0 <= ii <= 10
